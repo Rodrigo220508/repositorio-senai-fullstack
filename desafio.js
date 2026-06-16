9fa168269cfa47c6d51778d8b10e5f291f3e3b38
LEONARDO <leonardosantosbraga.58@gmail.com>
2026-06-16 18:34:41 -0300

    desafio feito

---
+++ desafio.js
@@ -0,0 +1,20 @@
+let peso = 71;
+
+let altura = 1.80;
+
+let IMC = peso / (altura * altura);
+console.log (IMC);
+
+let abaixoDoPeso = IMC < 18;5;
+
+if (abaixoDoPeso) {
+    console.log ("abaixo do peso")
+} else if (IMC >18.5 && IMC <24.9){
+    console.log ("peso normal")
+
+} else if (IMC >25 && IMC <29.9){
+    console.log ("sobre peso")
+
+} else {
+    console.log ("obesidade")
+}
